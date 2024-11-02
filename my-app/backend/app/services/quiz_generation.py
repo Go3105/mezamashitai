1b8fba5a2f899a76e0b4b8371159e2b8651b8c96
Go3105 <sato.go@ohsuga.lab.uec.ac.jp>
2024-11-03 03:34:01 +0900

    AlarmContext.tsx内のエラーを修正

--- my-app/backend/app/services/quiz_generation.py
+++ my-app/backend/app/services/quiz_generation.py
@@ -1,1 +1,17 @@
 #LLMを使って当日の予定に関するクイズを4択で生成、答えもLLMに生成してもらう
+from transformers import pipeline
+#from get_google_calendar import fetch_today_event
+
+
+pipe = pipeline("text-generation", model="tokyotech-llm/Llama-3-Swallow-8B-v0.1")
+
+# 生成したいテキストのプロンプトを入力
+prompt = "明日の天気は"
+
+# テキスト生成
+result = pipe(prompt, max_new_tokens=50, num_return_sequences=1)
+
+# 結果を表示
+print(result[0]["generated_text"])
+def generate_quiz():
+    return
