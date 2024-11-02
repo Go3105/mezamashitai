f63f43b701f448638fe915f054979050d03f2b7b
Hina03 <hinano.326.20@gmail.com>
2024-11-02 21:34:47 +0900

    アラーム追加画面作成（表示されない）

---
+++ my-app/src/app/alarm/alarmAddPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AlarmAdd from "../_components/AlarmAdd";
+
+const AlarmAddPage = () => {
+    return(
+        <div>
+            <AlarmAdd />
+        </div>
+    );
+};
+
+export default AlarmAddPage;
--- my-app/src/app/alarm/page.tsx
+++ my-app/src/app/alarm/page.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import AlarmList from "../_components/AlarmList";
+import AlarmAdd from "../_components/AlarmAdd";
+import Link from "next/link";
 
 export default function AlarmPage() {
   return (
     <div>
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl font-bold">アラーム</h1>
+        <Link href="/alarm/alarmAddPage">
+            <button className="bg-blue-500 text-white px-4 py-2 rounded">アラームを新規作成 ＋</button>
+        </Link>
+      </div>
       <AlarmList />
     </div>
   );
